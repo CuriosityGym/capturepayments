2037e981d5b35b56c063dcc2ca28af582060b42f
rupin <rupin.chheda@gmail.com>
2018-03-12 16:42:01 +0530

    Fc

--- capturepayments.py
+++ capturepayments.py
@@ -7,7 +7,7 @@ client = razorpay.Client(auth=(razorpay_key_id, razorpay_key_secret))
 
 payment_amount = 1250000
 resp = client.payment.fetch_all()
-print(resp)
+#print(resp)
 while True:
     for payment in resp["items"]:
         #print(payment)
@@ -15,7 +15,9 @@ while True:
         isCaptured=payment["captured"]
         payment_id=payment["id"]
         status=payment["status"]
-        print(payment_id + " " +str(isCaptured) + " "+ status)
+        print(payment_id)
+        print(str(isCaptured))
+        print(status)
         #print(status=="success")
         #if(not isCaptured and status!="failed"):        
         #    print(payment_id)
